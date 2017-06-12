0de3748c3d708fa0e94a7ff6482bee5b35c19317
Vugar Mammadli <v-m13@windowslive.com>
2017-06-12 03:55:59 -0400

    new

--- solution.py
+++ solution.py
@@ -70,19 +70,22 @@ def heur_alternate(state):
       
       else:
         
-        if(two_sides_blocked(state, snowball)):
-          return float('inf')
+        #if(two_sides_blocked(state, snowball)):
+          #return float('inf')
         
-        # checks if a snowball is in the one of the corners
-        if ((x == 0 and y == 0) or (x == 0 and y == state.height - 1)
-           or (x == state.width - 1 and y == 0) or (x == state.width - 1 and y == state.height - 1)):
-          if (state.destination != (x, y)):
-            return float('inf')        
+        ## checks if a snowball is in the one of the corners
+        #if ((x == 0 and y == 0) or (x == 0 and y == state.height - 1)
+           #or (x == state.width - 1 and y == 0) or (x == state.width - 1 and y == state.height - 1)):
+          #if (state.destination != (x, y)):
+            #return float('inf')        
         
-        # checks if snowball is in the beside of a side of wall and the destination is on that wall
-        if((x == 0 or x == state.width - 1) and x != state.destination[0]):
-          return float('inf')
-        elif((y == 0 or y == state.height - 1) and y != state.destination[1]):
+        ## checks if snowball is in the beside of a side of wall and the destination is on that wall
+        #if((x == 0 or x == state.width - 1) and x != state.destination[0]):
+          #return float('inf')
+        #elif((y == 0 or y == state.height - 1) and y != state.destination[1]):
+          #return float('inf')
+        
+        if (dead_corner_checker(state, snowball)):
           return float('inf')
         
         distance = abs(x - state.destination[0]) + abs(y - state.destination[1])
@@ -96,7 +99,55 @@ def heur_alternate(state):
         total = total + distance
     # manhattan distance for robot
     total += abs(state.robot[0] - state.destination[0]) + abs(state.robot[1] - state.destination[1])
-    return total  
+    return total 
+  
+  
+def dead_corner_checker(state, cord):
+  # bools for surrounding obstacle
+  up = False
+  down = False
+  left = False
+  right = False
+
+  # surrouding cordinates
+  up_dir = (cord[:1] + ((cord[1] - 1),))
+  down_dir = (cord[:1] + ((cord[1] + 1),))
+  left_dir = (((cord[0] - 1),) + cord[1:])
+  right_dir = (((cord[0] + 1),) + cord[1:])
+  dia_dir = (((cord[0] + 1),) + ((cord[1] + 1),))
+
+  # surrounding box condition bools
+  left_has_box = (left_dir in state.)
+  right_has_box = (right_dir in state.snowballs)
+  up_has_box = (up_dir in state.snowballs)
+  down_has_box = (down_dir in state.snowballs)
+  dia_has_box = (dia_dir in state.snowballs)
+
+  # check surrounding obstacles
+  if(cord[1] == 0):
+    up = True
+  elif (up_dir in state.obstacles):
+    up = True
+  if (cord[1] == (state.height - 1)):
+    down = True
+  elif (down_dir in state.obstacles):
+    down = True
+  if(cord[0] == 0):
+    left = True
+  elif (left_dir in state.obstacles):
+    left = True
+  if (cord[0] == (state.width - 1)):
+    right = True
+  elif (right_dir in state.obstacles):
+    right = True
+
+  # if deadlock happened
+  if ((cord not in state.destination) and 
+        (((up or down) and (left_has_box or right_has_box or left or right)) or ((left or right) and (up_has_box or down_has_box)) or
+             (down_has_box and right_has_box and dia_has_box))):  
+    return True
+  else:
+    return False
   
 def two_sides_blocked(state, snowball):
   '''INPUT: a snowball state, coordinates of a snowball'''
@@ -112,19 +163,18 @@ def two_sides_blocked(state, snowball):
       #return True
   
   # checks if there is an obstacle in two sides of a snowball
-  if(state.destination != (x, y)):
     # up and left
-    if (((x - 1, y) in state.obstacles) and ((x, y - 1) in state.obstacles)):
-      return True
-    # down and left
-    if (((x - 1, y) in state.obstacles) and ((x, y + 1) in state.obstacles)):
-      return True
-    # up and right
-    if (((x + 1, y) in state.obstacles) and ((x, y - 1) in state.obstacles)):
-      return True
-    # down and right
-    if (((x + 1, y) in state.obstacles) and ((x, y + 1) in state.obstacles)):
-      return True
+  if (((x - 1, y) in state.obstacles) and ((x, y - 1) in state.obstacles)):
+    return True
+  # down and left
+  if (((x - 1, y) in state.obstacles) and ((x, y + 1) in state.obstacles)):
+    return True
+  # up and right
+  if (((x + 1, y) in state.obstacles) and ((x, y - 1) in state.obstacles)):
+    return True
+  # down and right
+  if (((x + 1, y) in state.obstacles) and ((x, y + 1) in state.obstacles)):
+    return True
   
   return False
 
